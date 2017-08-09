72214d2145a890bbfe7f42b6aa78220e2c388bcb
rayGreycloud <rayGreycloud@gmail.com>
2017-08-09 13:06:27 -0700

    Refactor currency convert function using async/await

--- currency-convert.js
+++ currency-convert.js
@@ -30,7 +30,16 @@ const convertCurrency = (from, to, amount) => {
   });
 };
 
-convertCurrency('USD', 'CAD', 100).then((status) => {
+// Refactor using async/await
+const convertCurrencyAlt = async (from, to, amount) => {
+  const countries = await getCountries(to);
+  const rate = await getExchangeRate(from, to);
+  const exchangedAmount = amount * rate;
+
+  return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(', ')}`;
+};
+
+convertCurrencyAlt('USD', 'CAD', 100).then((status) => {
   console.log(status);
 });
 
