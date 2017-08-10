959941df14b84ecec050774358a0307d47c70504
rayGreycloud <rayGreycloud@gmail.com>
2017-08-10 10:27:17 -0700

    Add error handling

--- currency-convert.js
+++ currency-convert.js
@@ -5,15 +5,23 @@
 const axios = require('axios');
 
 const getExchangeRate = async (from, to) => {
-  const response = await axios.get(`http://api.fixer.io/latest?base=${from}`);
+  try {
+    const response = await axios.get(`http://api.fixer.io/latest?base=${from}`);
 
-  return response.data.rates[to];
+    return response.data.rates[to];
+  } catch(e) {
+    throw new Error(`Unable to find the exchange rate for ${from} to ${to}`);
+  }
 };
 
 const getCountries = async (currencyCode) => {
-  const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
 
-  return response.data.map((country) => country.name);
+    return response.data.map((country) => country.name);
+  } catch(e) {
+    throw new Error(`Unable to find any countries that use currency code: ${currencyCode}.`);
+  }
 };
 
 const convertCurrency = (from, to, amount) => {
@@ -30,15 +38,17 @@ const convertCurrency = (from, to, amount) => {
 
 // Refactor using async/await
 const convertCurrencyAlt = async (from, to, amount) => {
-  const countries = await getCountries(to);
   const rate = await getExchangeRate(from, to);
+  const countries = await getCountries(to);
   const exchangedAmount = amount * rate;
 
   return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(', ')}`;
 };
 
-convertCurrencyAlt('USD', 'CAD', 100).then((status) => {
+convertCurrencyAlt('USD', 'EUR', 100).then((status) => {
   console.log(status);
+}).catch((e) => {
+  console.log(e.message);
 });
 
 // getExchangeRate('USD', 'EUR').then((rate) => {
